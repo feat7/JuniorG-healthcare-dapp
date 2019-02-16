833866972f5c99b66a043b44e3aaf4626304331c
feat7 <vinaykhobragade99@gmail.com>
2019-02-17 02:49:21 +0530

    update: auth utility

--- utils/auth.js
+++ utils/auth.js
@@ -1,24 +1,32 @@
-import axios from 'axios';
-import {apiServer} from '../config';
-import clientPersist from "client-persist";
-import { get } from '.';
+import axios from "axios";
+import { apiServer } from "../config";
+import { runInAction } from "mobx";
 
-clientPersist.setDriver(clientPersist.SESSIONSTORAGE);
-
-export const register = async (userDetails) => {
-    return axios.post(`${apiServer}/api/users`, { user: userDetails }).then(response => response).catch(e => {
-        clientPersist.setItem('errorMessage', get(["data", "message"])(e.response)).then(() => console.log('--saved--'));
-        return false;
-    });
+export const login = (userDetails, store) => {
+  const { user } = store;
+  return axios
+    .post(`${apiServer}/api/users/login`, {
+      user: userDetails
+    })
+    .then(response =>
+      runInAction(() => {
+        user.details = response.data;
+        user.authToken = response.data.token;
+        return true;
+      })
+    )
+    .catch(e => false);
 };
 
-export const login = async (userDetails) => {
-    return axios.post(`${apiServer}/api/users/login`, { user: userDetails }).then(response => {
-        clientPersist.setItem('authToken', get(["data", "token"])(response)).then(() => console.log('--saved--'));
-        clientPersist.setItem('userId', get(["data", "_id"])(response)).then(() => console.log('--saved--'));
-        return true;
-    }).catch(e => {
-        clientPersist.setItem('errorMessage', get(["data", "message"])(e.response)).then(() => console.log('--saved--'));
-        return true;
-    });
+export const register = (userDetails, store) => {
+  const { user } = store;
+  return axios
+    .post(`${apiServer}/api/users`, {
+      user: userDetails
+    })
+    .then(response => {
+      user.details = response.data;
+      return true;
+    })
+    .catch(e => false);
 };
