4ebc4e37a7e51d579eccd9b7ff70e1fff08ac113
Kuldeep Pisda <pisdak79@gmail.com>
2019-02-16 20:56:17 +0530

    Commit: Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -14,16 +14,67 @@ export default class Main extends Component {
             <React.Fragment>
                 <Header/>
                 <body>
-                    <section className="section">
-                        <div className="container">
-                            <h1 className="title">
-                                Hello World
-                            </h1>
-                            <p className="subtitle">
-                                My first website with <strong>Bulma</strong>!
-                            </p>
+                    <nav className="navbar" role="navigation" aria-label="main navigation">
+                        <div className="navbar-brand">
+                            <a className="navbar-item" href="/">
+                                <img src="./static/images/logo.png" />
+                            </a>
+
+                            <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false"
+                               data-target="navbarBasicExample">
+                                <span aria-hidden="true"></span>
+                                <span aria-hidden="true"></span>
+                                <span aria-hidden="true"></span>
+                            </a>
                         </div>
-                    </section>
+
+                        <div id="navbarBasicExample" className="navbar-menu">
+                            <div className="navbar-start">
+                                <a className="navbar-item">
+                                    Junior G
+                                </a>
+
+                                <a className="navbar-item">
+                                    Home
+                                </a>
+
+                                <a className="navbar-item">
+                                    Track Status
+                                </a>
+
+                            </div>
+
+                            <div className="navbar-end">
+                                <div className="navbar-item">
+                                    <div className="buttons">
+                                        <a className="button is-primary">
+                                            <strong>Sign up</strong>
+                                        </a>
+                                        <a className="button is-light">
+                                            Log in
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </nav>
+                    <div className="columns">
+                        <div className="column">
+                            <img src="./static/images/donate.svg" />
+                        </div>
+                        <div className="column">
+                            <section className="hero">
+                                <div className="hero-body">
+                                    <h1 className="title">
+                                        Hero title
+                                    </h1>
+                                    <h2 className="subtitle">
+                                        Hero subtitle
+                                    </h2>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
                 </body>
             </React.Fragment>
         );
