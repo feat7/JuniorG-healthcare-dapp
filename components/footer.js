4242bc9e2b6e3a0794047da94622fdee9588ef3e
feat7 <vinaykhobragade99@gmail.com>
2019-02-16 22:55:37 +0530

    fix: footer

--- components/footer.js
+++ components/footer.js
@@ -7,7 +7,7 @@ export default class Footer extends React.Component {
                 <footer class="footer">
                     <div class="content has-text-centered">
                         <p>
-                        <strong>Dapp</strong> by <a href="https://jgthms.com">Team Junior G</a>. .
+                        <strong>Dapp</strong> by <a href="https://github.com/feat7/JuniorG-healthcare-dapp">Team Junior G</a>. .
                         </p>
                     </div>
                 </footer>
