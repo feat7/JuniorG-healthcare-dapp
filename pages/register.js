5d94afde64d994fe18286d9a2846fbf080c03075
feat7 <vinaykhobragade99@gmail.com>
2019-02-16 23:43:23 +0530

    register setup

--- pages/register.js
+++ pages/register.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import clientPersist from "client-persist";
 import Header from '../components/header';
 import Footer from '../components/footer';
+import { register } from '../utils/auth';
+
+clientPersist.setDriver(clientPersist.SESSIONSTORAGE);
 
 export default class Register extends React.Component {
     state = {
         email: '',
-        password: ''
+        password: '',
+        errorMessage: ''
     };
 
     render() {
@@ -17,22 +22,30 @@ export default class Register extends React.Component {
                         <div className="container">
                             <div className="columns">
                                 <div className="column">
-                                    <div class="field">
-                                        <div class="control">
-                                            <input value={this.state.email} class="input" type="text" placeholder="Email" onChange={(e) => {
+                                    <div className="field">
+                                        <div className="control">
+                                            <input value={this.state.email} className="input" type="text" placeholder="Email" onChange={(e) => {
                                                 this.setState({ email: e.target.value })
                                             }}/>
                                         </div>
                                     </div>
-                                    <div class="field">
-                                        <div class="control">
-                                            <input value={this.state.password} class="input" type="password" placeholder="Password" onChange={(e) => {
+                                    <div className="field">
+                                        <div className="control">
+                                            <input value={this.state.password} className="input" type="password" placeholder="Password" onChange={(e) => {
                                                 this.setState({ password: e.target.value })
                                             }}/>
                                         </div>
                                     </div>
-                                    <div onClick={() => {
-                                        console.log(this.state, 'Register');
+                                    <div className="has-text-danger">{this.state.errorMessage}</div>
+                                    <div onClick={async () => {
+                                        const success = await register({ email: this.state.email, password: this.state.password });
+                                        if(success) {
+                                            this.setState({ errorMessage: 'Successfully Registered' });
+                                        } else {
+                                            clientPersist.getItem("errorMessage").then(errorMessage => {
+                                                if (errorMessage !== null) this.setState({ errorMessage });
+                                            });
+                                        }
                                     }} className="button is-primary">Register</div>
                                 </div>
                                 <div className="column">
